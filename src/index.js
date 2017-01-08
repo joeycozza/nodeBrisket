c0efeb5a16ff5dfed3c8be5b6779f56516f853ac
Joey Cozza <joeycozza@gmail.com>
2017-01-07 19:52:23 -0700

    an okay flow for flow I guess

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // @flow
 'use strict';
+const test = require('./files/test1');
 
 function multiply(a: number, b: number) : number {
   return a * b;
 }
 
-console.log(multiply('as', 4));
+console.log(multiply(1, 4));
